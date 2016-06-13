98ff7c69cac0ac46a9deb67485cb3dc214b03832
郑露 <zhenglu@zhengludeMacBook-Pro.local>
2016-06-13 16:42:06 +0800

    限流工具类。简单粗暴，足够满足业务

---
+++ src/main/java/zbuer/com/limiter/ApplicationLimiter.java
@@ -0,0 +1,92 @@
+package zbuer.com.limiter;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 应用级限流
+ *
+ * @author buer
+ * @since 16/6/13
+ */
+public class ApplicationLimiter {
+
+	private AtomicLong atomicLong = new AtomicLong(0);
+
+	private static final int LIMIT = 1000;
+
+	/**
+	 * <pre>
+	 *     限制某个接口的总并发数/请求数（伪代码）
+	 * </pre>
+	 */
+	public void handleByLimiter() {
+		try {
+			if (atomicLong.incrementAndGet() > LIMIT) {
+				// 拒绝请求
+			}
+			//处理请求
+
+		} finally {
+			atomicLong.decrementAndGet();
+
+		}
+	}
+
+	/**
+	 * 时间窗口内限制并发数(限制某个接口/服务每秒/每分钟/每天的请求数/调用量)
+	 */
+
+	public void handleByLimiterAtFiXedTime() throws Exception {
+
+		LoadingCache<Long, AtomicLong> counter = CacheBuilder.newBuilder().expireAfterWrite(2, TimeUnit.SECONDS)
+		                                                     .build(new CacheLoader<Long, AtomicLong>() {
+
+			                                                     @Override
+			                                                     public AtomicLong load(Long key) throws Exception {
+				                                                     return new AtomicLong(0);
+			                                                     }
+		                                                     });
+
+		long limit = 1000;
+		while (true) {
+			//得到当前的秒数
+			long currentSeconds = System.currentTimeMillis() / 1000;
+			if (counter.get(currentSeconds).incrementAndGet() > limit) {
+
+				System.out.println("限流了：" + currentSeconds);
+				continue;
+			}
+			//处理业务逻辑
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		LoadingCache<Long, AtomicLong> counter = CacheBuilder.newBuilder().expireAfterWrite(2, TimeUnit.SECONDS)
+		                                                     .build(new CacheLoader<Long, AtomicLong>() {
+
+			                                                     @Override
+			                                                     public AtomicLong load(Long key) throws Exception {
+				                                                     return new AtomicLong(0);
+			                                                     }
+		                                                     });
+
+		for (int i = 0; i < 2000; i++) {
+			long currentSeconds = System.currentTimeMillis() / 1000;
+			System.out.println("current :" + currentSeconds);
+			System.out.println(counter.get(currentSeconds).incrementAndGet());
+			if(counter.get(currentSeconds).incrementAndGet()  > 1000){
+				System.out.println("限流啦啦");
+				Thread.sleep(1000);
+			}
+
+		}
+
+	}
+
+}
